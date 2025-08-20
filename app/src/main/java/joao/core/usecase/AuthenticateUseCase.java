a14fd8510181d7cfaa9a124f3415a66170704f91
João Oliveira <xliveirvwrld@gmail.com>
2025-08-20 08:37:45 -0300

    feat: fix import error

--- app/src/main/java/joao/core/usecase/AuthenticateUseCase.java
+++ app/src/main/java/joao/core/usecase/AuthenticateUseCase.java
@@ -2,6 +2,7 @@ package joao.core.usecase;
 
 import joao.adapter.in.web.dto.LoginRequest;
 import joao.adapter.in.web.dto.LoginResponse;
+import joao.config.AwsJwtSecretConfig;
 import joao.config.JwtConfig;
 import joao.core.exception.LoginException;
 import joao.core.port.in.AuthenticatePortIn;
@@ -16,15 +17,15 @@ public class AuthenticateUseCase implements AuthenticatePortIn {
     private final UserRepositoryPortOut userRepositoryPortOut;
     private final PasswordEncoder passwordEncoder;
     private final JwtServicePortOut jwtServicePortOut;
-    private final JwtConfig jwtConfig;
+    private final AwsJwtSecretConfig awsJwtSecretConfig;
 
     public AuthenticateUseCase(UserRepositoryPortOut userRepositoryPortOut,
                                PasswordEncoder passwordEncoder,
-                               JwtServicePortOut jwtServicePortOut, JwtConfig jwtConfig) {
+                               JwtServicePortOut jwtServicePortOut, AwsJwtSecretConfig awsJwtSecretConfig) {
         this.userRepositoryPortOut = userRepositoryPortOut;
         this.passwordEncoder = passwordEncoder;
         this.jwtServicePortOut = jwtServicePortOut;
-        this.jwtConfig = jwtConfig;
+        this.awsJwtSecretConfig = awsJwtSecretConfig;
     }
 
     @Override
@@ -41,6 +42,6 @@ public class AuthenticateUseCase implements AuthenticatePortIn {
 
         String token = jwtServicePortOut.generateToken(user);
 
-        return new LoginResponse(token, jwtConfig.getExpiresIn()); // 30 min em milissegundos
+        return new LoginResponse(token, awsJwtSecretConfig.jwtExpiresIn()); // 30 min em milissegundos
     }
 }
